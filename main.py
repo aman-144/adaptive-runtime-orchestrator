7a50d2b6d54c3794efc7cffd08ec9015e962c0cb
aman <codingyakuza@gmail.com>
2026-05-07 18:12:07 +0530

    Implement disk telemetry and validate runtime monitoring accuracy

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import psutil
 import time
 
-while True: 
+
+while True:
   cpu_usage = psutil.cpu_percent()
-  print(f"cpu usage:{cpu_usage}% ")
+  ram_usage = psutil.virtual_memory().percent
+  disk_usage = psutil.disk_usage('/').percent
+  total_ram = psutil.virtual_memory().total
+  used_ram = psutil.virtual_memory().used
+  free_ram = psutil.virtual_memory().free
+  print(f"cpu usage:{cpu_usage}% , ram usage:{ram_usage}% , disk usage:{disk_usage}% ")
+  print(f"total ram:{total_ram} , used ram:{used_ram} , free ram:{free_ram}")
+ 
   time.sleep(1)
